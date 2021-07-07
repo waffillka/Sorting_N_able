55f9cf80ac2c6227bb871416b7cd1c2ab0e95b7e
“Sebik <waffilka@gmail.comSebik Georgegit config --global user.email waffilka@gmail.com>
2021-07-07 23:07:37 +0300

    implemented singly linked list

---
+++ Sorting_N_able/CustomList/CustomList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_N_able.CustomList
+{
+    class CustomList<T>
+    {
+        private Node<T> head;
+        public int Size { get; private set; }
+
+        public CustomList()
+        {
+            this.Size = 0;
+            this.head = null;
+        }
+
+        public bool Empty() => head == null;
+
+        public void Push_front(T info)
+        {
+            this.head = new Node<T>(info, head);
+            this.Size++;
+        }
+
+        public T Pop_front()
+        {
+            if (head is null)
+                return default;
+
+            T item = head.Item;
+            head = head.Next;
+            this.Size--;
+
+            return item;
+        }
+
+        public void Push_back(T info)
+        {
+            if (head is null)
+                Push_front(info);
+
+            var temp = head;
+            while(temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+
+            temp.Next = new Node<T>(info, null);
+        }
+
+        public T Pop_back()
+        {
+            if (head is null)
+                return default;
+
+            Node<T> temp, temp2;
+            temp2 = null;
+            temp = head;
+            while(temp.Next != null)
+            {
+                temp2 = temp;
+                temp = temp.Next;
+            }
+
+            T item;
+
+            if (temp2 == null)
+            {
+                item = head.Item;
+            }
+            else
+            {
+                temp2.Next = null;
+                item = temp.Item;
+            }
+
+            this.Size--;
+
+            return item;
+        }
+
+        public IEnumerable<T> ToList()
+        {
+            Node<T> node = head;
+            while (node != null)
+            {
+                yield return node.Item;
+                node = node.Next;
+            }
+        }
+    }
+}
---
+++ Sorting_N_able/CustomList/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_N_able.CustomList
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Item { get; }
+
+        public Node(T item, Node<T> node)
+        {
+            Item = item;
+            Next = node;
+        }
+    }
+}
